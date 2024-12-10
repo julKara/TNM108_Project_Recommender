c552f9fa8a15514a12f6f8807995356794ae54d3
RainbowSapling <mirbj463@student.liu.se>
2024-12-10 16:28:54 +0100

    test file

--- testMirijam.py
+++ testMirijam.py
@@ -11,11 +11,12 @@ from summa import keywords
 
 # Load the train and test datasets to create two DataFrames
 
-reviews = pd.read_csv("./rotten_tomatoes_critic_reviews.csv")
+reviews = pd.read_csv("data/rotten_tomatoes_critic_reviews.csv")
 
-movies = pd.read_csv("./rotten_tomatoes_movies.csv")
+movies = pd.read_csv("data/rotten_tomatoes_movies.csv")
 
-index = 1
+
+index = 230
 
 agg_functions = {'rotten_tomatoes_link': 'first', 'review_content': 'sum', }
 df_new = reviews.groupby(reviews['rotten_tomatoes_link']).aggregate(agg_functions)
@@ -23,12 +24,12 @@ revText = df_new["review_content"]
 
 print(len(revText))
 
-print(revText.iloc(index))
+print(revText[index])
 
-#print(df_new['rotten_tomatoes_link'][index])
+print(df_new['rotten_tomatoes_link'][index])
 
-#print("Keywords:\n",keywords.keywords(revText[index]))
+print("Keywords:\n",keywords.keywords(revText[index]))
 
-#id = df_new['rotten_tomatoes_link'][index] == movies['rotten_tomatoes_link']
+id =  movies['rotten_tomatoes_link'] == df_new['rotten_tomatoes_link'][index]
 
-#print(movies['movie_title'][id])
+print(movies['movie_title'][id])
